6d2cf0c43b508486010af4685cf3461291bc0ecb
Eugene <89818121788@mail.ru>
2022-07-13 23:22:46 +0300

    first commit

---
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.example.calculator;
+
+
+
+
+public class CalculatorService {
+    public int servicePlus(int number1, int number2) {
+        int sum = number1 + number2;
+        return sum;
+    }
+}
